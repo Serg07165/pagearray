aa8b1cccb61ce4c2067ae600bf2b89d7f6f0a5ac
Serg07165 <lg02121497@gmail.com>
2025-04-13 12:23:54 +0300

    готовый проект

--- 2/views/page.php
+++ 2/views/page.php
@@ -3,7 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title>Главная страница</title>
-    
+    <style>
+        .gallery-link {
+            margin-top: 20px; 
+            margin-bottom: 20px; 
+            display: inline-block; 
+            text-decoration: none; 
+            font-size: 1.2em;
+            color: #007BFF; 
+        }
+
+        .gallery-link:hover {
+            text-decoration: underline; 
+        }
+    </style>
 </head>
 <body>
    <h1><?=$hello?></h1>
